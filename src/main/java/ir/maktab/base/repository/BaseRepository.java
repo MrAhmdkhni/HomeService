5fa68285de693353812ec81feafae45709a88b52
Amir <popularamirhosein@gmail.com>
2022-12-06 18:31:41 +0330

    complete BaseRepository interface and BaseRepositoryImpl class

---
+++ src/main/java/ir/maktab/base/repository/BaseRepository.java
@@ -0,0 +1,21 @@
+package ir.maktab.base.repository;
+
+import ir.maktab.base.entity.BaseEntity;
+import jakarta.persistence.EntityManager;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<E extends BaseEntity<ID>, ID extends Serializable> {
+
+    void saveOrUpdate(E e);
+
+    void delete(E e);
+
+    Optional<E> loadById(ID id);
+
+    List<E> loadAll();
+
+    EntityManager getEntityManager();
+}
---
+++ src/main/java/ir/maktab/base/repository/impl/BaseRepositoryImpl.java
@@ -0,0 +1,51 @@
+package ir.maktab.base.repository.impl;
+
+import ir.maktab.base.entity.BaseEntity;
+import ir.maktab.base.repository.BaseRepository;
+import jakarta.persistence.EntityManager;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class BaseRepositoryImpl<E extends BaseEntity<ID>, ID extends Serializable> implements BaseRepository<E, ID> {
+
+    protected final EntityManager em;
+
+    public BaseRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    public abstract Class<E> getEntityClass();
+
+    @Override
+    public void saveOrUpdate(E entity) {
+        if (entity.isNew()) {
+            em.persist(entity);
+        } else {
+            em.merge(entity);
+        }
+    }
+
+    @Override
+    public void delete(E e) {
+        em.remove(e);
+    }
+
+    @Override
+    public Optional<E> loadById(ID id) {
+        return Optional.ofNullable(em.find(getEntityClass(), id));
+    }
+
+    @Override
+    public List<E> loadAll() {
+        return em
+                .createQuery("from " + getEntityClass().getSimpleName(), getEntityClass())
+                .getResultList();
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+}
